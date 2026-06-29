c473d21ff24827c5aeda478574d4b3780ceb75d7
cucnau <cucnau01@gmail.com>
2026-06-29 17:46:03 +0700

    Update ChucuGame.tsx

--- src/pages/ChucuGame.tsx
+++ src/pages/ChucuGame.tsx
@@ -1,15 +1,22 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useStore } from '../store';
+import { useFeatureRestriction } from '../types/features';
 
 export function ChucuGame() {
   const navigate = useNavigate();
   const setChucuGameOpen = useStore(state => state.setChucuGameOpen);
+  const setLockedFeatureId = useStore(state => state.setLockedFeatureId);
+  const { isFeatureLocked } = useFeatureRestriction();
 
   useEffect(() => {
-    setChucuGameOpen(true);
+    if (isFeatureLocked('chucu_catch')) {
+      setLockedFeatureId('chucu_catch');
+    } else {
+      setChucuGameOpen(true);
+    }
     navigate('/', { replace: true });
-  }, [navigate, setChucuGameOpen]);
+  }, [navigate, setChucuGameOpen, isFeatureLocked, setLockedFeatureId]);
 
   return (
     <div className="flex-1 flex items-center justify-center min-h-[50vh] text-[#3E2723]">
